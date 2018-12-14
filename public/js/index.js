f551ccaba2daff7e715aa23bcd606d24d103c3d8
Yuan Zhi <zy8715@mun.ca>
2018-12-13 23:55:10 -0330

    Emit new message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from:'Waye@example.com',
-        text: 'Hey, This is Waye.'
-    });
 });
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
