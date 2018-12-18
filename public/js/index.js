3e70e9c574856f0393174f1ff3989a8be9a1d23d
Yuan Zhi <zy8715@mun.ca>
2018-12-18 15:53:20 -0330

    Add Matache for message

--- public/js/index.js
+++ public/js/index.js
@@ -8,22 +8,30 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    jQuery('#message').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a')
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template,{
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a')
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-
-    li.text(`${message.from} ${formattedTime}:`);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#message').append(li);
+    //var li = jQuery('<li></li>');
+   // var a = jQuery('<a target="_blank">My current location</a>');
+    var template = jQuery('#location-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    // li.text(`${message.from} ${formattedTime}:`);
+    // a.attr('href', message.url);
+    // li.append(a);
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
